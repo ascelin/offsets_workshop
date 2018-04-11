046bdc320539f48cb196d671f37ae09370568460
isaacpeterson <isaac.p.mail@gmail.com>
2018-04-12 08:08:03 +1000

    update plot lies to match total upper bounds

--- plot_elicitations.R
+++ plot_elicitations.R
@@ -3,17 +3,17 @@ library(googlesheets)
 library(plyr)
 library(abind)
 
-overlay_plot_list <- function(plot_type, plot_list, x_vec, yticks, ylims, heading, ylab, x_lab, col_vec, lty_vec, lwd_vec, legend_vec, legend_loc){
+overlay_plot_list <- function(plot_type, plot_list, x_vec, yticks, y_lims, heading, ylab, x_lab, col_vec, lty_vec, lwd_vec, legend_vec, legend_loc){
   
   if (plot_type == 'non-overlay'){
-    graphics::plot(y = plot_list[[1]], x = x_vec, type = 'l', main = heading, ylim = ylims, ylab = ylab, xlab = x_lab, col = col_vec[1], lty = lty_vec[1], lwd = lwd_vec[1])
+    graphics::plot(y = plot_list[[1]], x = x_vec, type = 'l', main = heading, ylim = y_lims, ylab = ylab, xlab = x_lab, col = col_vec[1], lty = lty_vec[1], lwd = lwd_vec[1])
   } else {
-    lines(y = plot_list[[1]], x = x_vec, type = 'l', main = heading, ylim = ylims, ylab = ylab, xlab = x_lab, col = col_vec[1], lty = lty_vec[1], lwd = lwd_vec[1])
+    lines(y = plot_list[[1]], x = x_vec, type = 'l', main = heading, ylim = y_lims, ylab = ylab, xlab = x_lab, col = col_vec[1], lty = lty_vec[1], lwd = lwd_vec[1])
   }
   
   if (length(plot_list) > 1){
     for (plot_ind in 2:length(plot_list)){
-      lines(y = plot_list[[plot_ind]],  x = x_vec, ylim = ylims, col = col_vec[plot_ind], lwd = lwd_vec[plot_ind], lty = lty_vec[plot_ind])
+      lines(y = plot_list[[plot_ind]],  x = x_vec, ylim = y_lims, col = col_vec[plot_ind], lwd = lwd_vec[plot_ind], lty = lty_vec[plot_ind])
     }
   }
   abline(h = 0, lty = 2)
@@ -34,41 +34,43 @@ setup_sub_plots <- function(nx, ny, x_space, y_space){
 
 #gs_auth(new_user = TRUE)
 
-include_random_data = TRUE
-author_num = 6
+include_random_data = FALSE
+author_num = 2
 author_col = c('red', 'darkgreen', 'pink', 'green', 'orange', 'blue')
 
 write_pdf = TRUE
 output_pdf_filename = '~/Documents/CPW_elicitation_1.pdf'
-worksheets_to_use = 2:3
+worksheets_to_use = c(2, 6)
 
 sheet_num = length(worksheets_to_use)
-plot_lwd = 1
+plot_lwd = 2
 plot_lty = 1
-mean_plot_lwd = 2
+mean_plot_lwd = 3
 mean_plot_lty = 2
 
 column_vec = c(2, 3, 4)
 time_vec = c(0, 20, 40, 60)
 lty_vec = c(1, 1, 2)
-ylims = c(0, 10)
+
 nx = 3
 ny = 2
 plot_x_space = 5
 plot_y_space = 5
-plot_selection_type = 'by_author'
+plot_selection_type = 'by_plot'
+plot_means = FALSE
 
+file_prefix = 'CPW_elicitation_master'
 sheet_characteristics = gs_ls()
-author_sheets_to_use = grepl('CPW_elicitation_isaac_edits', sheet_characteristics$sheet_title)
+author_sheets_to_use = grepl(file_prefix, sheet_characteristics$sheet_title)
 
 googlesheet_names = sheet_characteristics$sheet_title[author_sheets_to_use]
 
 column_names = c('Year', 'Lower Bound',	'Upper Bound',	'Best Estimate',	'Confidence 50-100 (%)',	
-              '90% CI (LB)',	'90% CI (UB)')
+              '90% CI (LB)',	'90% CI (UB)', 'Uppermost')
 
 for (author_ind in seq(author_num)){
   #current_sheet_name = googlesheet_names[author_ind]
-  current_sheet_name = googlesheet_names[1]
+  current_sheet_name = googlesheet_names
   current_sheet_characteristics = gs_title(current_sheet_name)
   current_worksheet_names = gs_ws_ls(current_sheet_characteristics)
   #for (current_sheet_ind in seq_along(worksheets_to_use)){
@@ -93,7 +95,7 @@ for (author_ind in seq(author_num)){
     current_filename = paste0('author_responses/author_', author_ind, '_sheet_', worksheets_to_use[current_sheet_ind], '.csv')
     
     current_sheet_data = read.csv(current_filename, na.strings=c("","NA"), stringsAsFactors = FALSE)
-    data_to_use = current_sheet_data[, 1:7]
+    data_to_use = current_sheet_data[, 1:8]
     names(data_to_use) = column_names
     plot_names[[current_sheet_ind]][[author_ind]] = data_to_use[, 1]
     current_data = data.matrix(data_to_use)
@@ -113,7 +115,7 @@ current_worksheet_names = current_worksheet_names[worksheets_to_use]
 
 numerical_data_matrix = lapply(seq_along(numerical_data), function(i) abind(numerical_data[[i]], along=3))
 
-sheet_means = lapply(seq_along(numerical_data), function(i) Reduce('+', numerical_data[[i]])/author_num)
+#sheet_means = lapply(seq_along(numerical_data), function(i) Reduce('+', numerical_data[[i]])/author_num)
 
 sheet_mins = lapply(seq_along(numerical_data), function(i) aaply(laply(numerical_data[[i]], as.matrix), c(2, 3), min, na.omit = TRUE))
 sheet_maxs = lapply(seq_along(numerical_data), function(i) aaply(laply(numerical_data[[i]], as.matrix), c(2, 3), max, na.omit = TRUE))
@@ -121,6 +123,7 @@ sheet_means = lapply(seq_along(numerical_data), function(i) aaply(laply(numerica
 
 plot_starts = lapply(seq_along(sheet_means), function(i) as.numeric(which(sheet_means[[i]][, 1] == 0)))
 
+sheet_y_lims = lapply(seq_along(sheet_maxs), function(i) c(0, max(sheet_maxs[[i]][, 8], na.rm = TRUE)))
 
 if (write_pdf == TRUE){
   pdf(output_pdf_filename, width = 11.7, height = 8.3)
@@ -128,29 +131,38 @@ if (write_pdf == TRUE){
 
 for (sheet_ind in 1:sheet_num){
   current_plot_starts = plot_starts[[sheet_ind]]
+  y_lims = sheet_y_lims[[sheet_ind]]
   if (plot_selection_type == 'by_plot'){
     setup_sub_plots(nx, ny = length(current_plot_starts), plot_x_space, plot_y_space)
   }
+  
   for (plot_ind in 1:length(current_plot_starts)){
     current_plot_vec = current_plot_starts[plot_ind]:(current_plot_starts[plot_ind] + 3)
     plot_list = lapply(seq_along(numerical_data[[sheet_ind]]), function(i) numerical_data[[sheet_ind]][[i]][current_plot_vec, ])
     current_mean_list = sheet_means[[sheet_ind]][current_plot_vec, ]
     if (plot_selection_type == 'by_author'){
-      setup_sub_plots(nx, ny, x_space = plot_x_space, y_space = plot_y_space)
       mean_plot_list = lapply(column_vec, function(i) current_mean_list[, i])
-      for (author_ind in seq(author_num)){
-        current_plot_list = lapply(column_vec, function(i) plot_list[[author_ind]][, i])
-        
-        current_plot_name = plot_names[[sheet_ind]][[author_ind]][current_plot_starts[plot_ind] - 2]
-        
-        overlay_plot_list(plot_type = 'non-overlay', current_plot_list, x_vec = time_vec, yticks = 'y', ylims, heading = current_plot_name, ylab = '', x_lab = '', 
-                          col_vec = rep(author_col[author_ind], 3), lty_vec, lwd_vec = rep(plot_lwd, length(plot_list)), 
-                          legend_vec = 'NA', legend_loc = FALSE)
-        
-        overlay_plot_list(plot_type = 'overlay', mean_plot_list, x_vec = time_vec, yticks = 'y', ylims, heading = current_plot_name, ylab = '', x_lab = '', 
+      if ('plot_means' == TRUE){
+        setup_sub_plots(nx = 1, ny = 1, x_space = plot_x_space, y_space = plot_y_space)
+        overlay_plot_list(plot_type = 'non-overlay', mean_plot_list, x_vec = time_vec, yticks = 'y', y_lims, heading = 'mean profiles', ylab = '', x_lab = '', 
                           col_vec = rep('black', 3), lty_vec, lwd_vec = rep(mean_plot_lwd, length(plot_list)), 
                           legend_vec = 'NA', legend_loc = FALSE)
-        
+      } else {
+        setup_sub_plots(nx, ny, x_space = plot_x_space, y_space = plot_y_space)
+        for (author_ind in seq(author_num)){
+          current_plot_list = lapply(column_vec, function(i) plot_list[[author_ind]][, i])
+          
+          current_plot_name = plot_names[[sheet_ind]][[author_ind]][current_plot_starts[plot_ind] - 2]
+          current_plot_name = gsub("Management scenario:", "", current_plot_name)
+          overlay_plot_list(plot_type = 'non-overlay', current_plot_list, x_vec = time_vec, yticks = 'y', y_lims, heading = current_plot_name, ylab = '', x_lab = '', 
+                            col_vec = rep(author_col[author_ind], 3), lty_vec, lwd_vec = rep(plot_lwd, length(plot_list)), 
+                            legend_vec = 'NA', legend_loc = FALSE)
+          
+          overlay_plot_list(plot_type = 'overlay', mean_plot_list, x_vec = time_vec, yticks = 'y', y_lims, heading = current_plot_name, ylab = '', x_lab = '', 
+                            col_vec = rep('black', 3), lty_vec, lwd_vec = rep(mean_plot_lwd, length(plot_list)), 
+                            legend_vec = 'NA', legend_loc = FALSE)
+          
+        }
       }
       
     } else if (plot_selection_type == 'by_plot'){
@@ -166,10 +178,10 @@ for (sheet_ind in 1:sheet_num){
           y_lab = ''
         }
         
-        overlay_plot_list(plot_type = 'non-overlay', current_plot_list, x_vec = time_vec, yticks = 'y', ylims, heading = column_names[col_ind], y_lab, x_lab = '', 
+        overlay_plot_list(plot_type = 'non-overlay', current_plot_list, x_vec = time_vec, yticks = 'y', y_lims, heading = column_names[col_ind], y_lab, x_lab = '', 
                           col_vec = author_col, lty_vec = rep(plot_lty, length(plot_list)), lwd_vec = rep(plot_lwd, length(plot_list)), 
                           legend_vec = 'NA', legend_loc = FALSE)
-        overlay_plot_list(plot_type = 'overlay', list(current_mean_list[, col_ind]), x_vec = time_vec, yticks = 'y', ylims, heading = column_names[col_ind], y_lab, x_lab = '', 
+        overlay_plot_list(plot_type = 'overlay', list(current_mean_list[, col_ind]), x_vec = time_vec, yticks = 'y', y_lims, heading = column_names[col_ind], y_lab, x_lab = '', 
                           col_vec = 'black', lty_vec = mean_plot_lty, lwd_vec = mean_plot_lwd, 
                           legend_vec = 'NA', legend_loc = FALSE)
       }
